0f698e5eb04fcc2b592aec989bf855e67620016d
abedkhateeb <abed.technion@gmail.com>
2015-01-22 18:51:43 +0200

    add delete route feature add clear map feature

--- app/src/main/java/followmeapp/followme/Route.java
+++ app/src/main/java/followmeapp/followme/Route.java
@@ -13,36 +13,46 @@ import com.google.android.gms.maps.model.PolylineOptions;
  * Created by abed on 12/29/2014.
  */
 public class Route {
-    static PolylineOptions polylineOptions=new PolylineOptions();
+    static PolylineOptions polylineOptions = new PolylineOptions();
     static boolean routing_mode = false;
-    static LatLng lastPoint =null;
+    static LatLng lastPoint = null;
     static double distance = 0.0;
-    static long time =0;
-    static String address="";
-    static void start_routing(){
+    static long time = 0;
+    static String address = "";
+
+    static void start_routing() {
         routing_mode = true;
-        polylineOptions=new PolylineOptions();
+        polylineOptions = new PolylineOptions();
         lastPoint = null;
         distance = 0.0;
-        time=0;
-        polylineOptions.color(Color.rgb(65,105,225)).width(10).visible(true);
+        time = 0;
+        polylineOptions.color(Color.rgb(65, 105, 225)).width(10).visible(true);
     }
 
-    static void stop_routing(){
+    static void stop_routing() {
         routing_mode = false;
     }
-    static boolean is_started(){
+
+    static boolean is_started() {
         return routing_mode;
     }
 
     public static double add_point(LatLng currentLocation) {
-        if (lastPoint!=null){
+        if (lastPoint != null) {
             float[] results = new float[1];
-            Location.distanceBetween(lastPoint.latitude,lastPoint.longitude,currentLocation.latitude,currentLocation.longitude,results);
-            distance += (results[0]/1000);
+            Location.distanceBetween(lastPoint.latitude, lastPoint.longitude, currentLocation.latitude, currentLocation.longitude, results);
+            distance += (results[0] / 1000);
         }
-        lastPoint = new LatLng(currentLocation.latitude,currentLocation.longitude);
+        lastPoint = new LatLng(currentLocation.latitude, currentLocation.longitude);
         polylineOptions.add(currentLocation);
         return distance;
+    }
+
+    public static void clear() {
+        polylineOptions = new PolylineOptions();
+        lastPoint = null;
+        distance = 0.0;
+        time = 0;
+        address = "";
     }
 }
